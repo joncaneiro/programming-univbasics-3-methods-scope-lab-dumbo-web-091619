aecaee5cf6f8bc3c3482d018d4c71a1e517ad87e
Jon Caneiro <joncaneiro@gmail.com>
2019-09-06 01:33:50 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,15 +4,22 @@ def mario
   puts phrase
 end
 
+
+
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
 
+
+
 def link 
   phrase = "It's Dangerous To Go Alone! Take This."
   puts phrase
 end
+
+
+
 
 Barrel = "Do A Barrel Roll!"
 
